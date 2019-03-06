ce9d095826ea8183d77fa0b57e3b87d953b8422a
ritafx <arg.freitas93@gmail.com>
2019-03-06 15:02:10 +0100

    updated graph animations mapped to scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-/***************************************GRAPH*********************************************/
+/*************************************** GRAPH *********************************************/
 
 var margin = {
         top: 60,
@@ -88,42 +88,6 @@ d3.csv("data/temperatureData.csv", function(error, data) {
     //     .call(transition);
 });
 
-d3.select("body")
-    .on("keydown", function() {
-        var section;
-        if (d3.event.keyCode == 49) {
-            //1 key press
-            start_index = 0;
-            end_index = 145;
-            section = allData.slice(start_index, end_index);
-
-            drawSection(section, line);
-
-        } else if (d3.event.keyCode == 50) {
-            //2 key press
-            start_index = 144;
-            end_index = 200;
-            section = allData.slice(start_index, end_index);
-
-            drawSection(section, line);
-
-        } else if (d3.event.keyCode == 51) {
-            //3 key press
-            start_index = 199;
-            end_index = 234;
-            section = allData.slice(start_index, end_index);
-
-            drawSection(section, line);
-
-        } else if (d3.event.keyCode == 52) {
-            //4 key press
-            start_index = 233;
-            end_index = 272;
-            section = allData.slice(start_index, end_index);
-
-            drawSection(section, line);
-        }
-    });
 
 function drawSection(data, line) {
     svg.append("path")
@@ -134,26 +98,176 @@ function drawSection(data, line) {
 }
 
 function transition(path) {
-    path.transition()
-        .duration(3000)
-        .attrTween("stroke-dasharray", tweenDash);
+    var totalLength = path.node().getTotalLength();
+
+    path.attr("stroke-dasharray", totalLength + " " + totalLength)
+        .attr("stroke-dashoffset", totalLength)
+        .transition()
+        .duration(2000)
+        .ease("linear")
+        .attr("stroke-dashoffset", 0);
+
+    // path.transition()
+    //     .duration(2000)
+    //     .attrTween("stroke-dasharray", tweenDash);
 }
 
-function tweenDash() {
-    var l = this.getTotalLength(),
-        i = d3.interpolateString("0," + l, l + "," + l);
-    return function(t) {
-        return i(t);
-    };
-}
+// function tweenDash() {
+//     var l = this.getTotalLength(),
+//         i = d3.interpolateString("0," + l, l + "," + l);
+//     return function(t) {
+//         return i(t);
+//     };
+// }
 
-// window.addEventListener('scroll', function() {
-//     var l = svg.select('path.line')[0][0].getTotalLength();
-//     svg.select('path.line').transition()
-//         // .duration(2000)
-//         .attr("stroke-dasharray", function() {
-//             return pageYOffset + ", " + l;
-//         });
-// });
+/************* Graph animations functions **************/
 
-/************************************************************************************/
+function animateGraphSection1() {
+    var start_index = 0;
+    var end_index = 145;
+    var section = allData.slice(start_index, end_index);
+
+    drawSection(section, line);
+};
+
+function animateGraphSection2() {
+    var start_index = 144;
+    var end_index = 200;
+    var section = allData.slice(start_index, end_index);
+
+    drawSection(section, line);
+};
+
+function animateGraphSection3() {
+    var start_index = 199;
+    var end_index = 234;
+    var section = allData.slice(start_index, end_index);
+
+    drawSection(section, line);
+};
+
+function animateGraphSection4() {
+    var start_index = 233;
+    var end_index = 272;
+    var section = allData.slice(start_index, end_index);
+
+    drawSection(section, line);
+};
+
+
+
+
+
+
+/***************************************** SCROLLIFY ***********************************************/
+
+
+$.scrollify({
+    section: ".panel",
+    scrollbars: false,
+    before: function(i, panels) {
+        var ref = panels[i].attr("data-section-name");
+        $(".pagination .active").removeClass("active");
+        $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+
+        /* the following lines will hide the navigation dots when the first or last page is active. Otherwise, they are made visible*/
+        var curSection = $.scrollify.current();
+        var curSecName = $($(curSection).get(0)).attr("data-section-name")
+
+        if (curSecName == "home" || curSecName == "end") {
+            $(".pagination").css({ "visibility": "hidden" });
+        } else {
+            $(".pagination").css({ "visibility": "visible" });
+        }
+
+    },
+    afterRender: function() {
+        var pagination = "<ul class=\"pagination\">";
+        var activeClass = "";
+        $(".panel").each(function(i) {
+            activeClass = "";
+            if (i === 0) {
+                activeClass = "active";
+            }
+
+            /*create for each page a navigation dot except for the first and last page*/
+            if ($(this).attr("data-section-name") != "home" && $(this).attr("data-section-name") != "end") {
+                pagination += "<li><a class=\"" +
+                    activeClass +
+                    "\" href=\"#" +
+                    $(this).attr("data-section-name") +
+                    "\"><span class=\"hover-text\">" +
+                    $(this).attr("data-section-name").charAt(0).toUpperCase() +
+                    $(this).attr("data-section-name").slice(1) +
+                    "</span></a></li>";
+            }
+        });
+
+        pagination += "</ul>";
+        $(".panel1").append(pagination);
+
+        /*
+			Tip: The two click events below are the same:
+			$(".pagination a").on("click",function() {
+			  $.scrollify.move($(this).attr("href"));
+			});
+	  
+			*/
+        $(".pagination a").on("click", $.scrollify.move);
+    },
+    after: function(i, panels) {
+        var section, start_index, end_index;
+        if (i == 1) {
+            animateGraphSection1();
+        } else if (i == 2) {
+            animateGraphSection2();
+        } else if (i == 3) {
+            animateGraphSection3();
+        } else if (i == 4) {
+            animateGraphSection4();
+        }
+    },
+});
+
+
+
+$(".scroll,.scroll-btn").click(function(e) {
+    e.preventDefault();
+    $.scrollify.next();
+});
+
+/* when document is loaded, hide the navigation dots */
+$(document).ready(function() {
+    $(".pagination").css({ "visibility": "hidden" });
+});
+
+
+
+/*
+ * jQuery Scrollify
+ * Version 1.0.19
+ *
+ * Requires:
+ * - jQuery 1.7 or higher
+ *
+ * https://github.com/lukehaas/Scrollify
+ *
+ * Copyright 2016, Luke Haas
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ */
