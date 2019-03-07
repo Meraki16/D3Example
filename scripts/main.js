31b9fa214ef1410206cbc1876e42f01f3e21060b
ritafx <arg.freitas93@gmail.com>
2019-03-07 20:17:03 +0100

    updated d3 version and main js changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,5 @@
 /*************************************** GRAPH *********************************************/
 
-
-/**** load all data ****/
-var allTempData;
-var stabilisedData;
-
-// get historic + hot house earth temperatures data
-d3.csv("data/temperatureData.csv", function(error, data) {
-    if (error) throw error;
-    console.log(data);
-    data.forEach(function(d) {
-        d.Year = d.Year;
-        d.Annomaly = +d.Annomaly;
-    });
-
-    allTempData = data;
-});
-
-// get stabilised earth temperatures data
-d3.csv("data/stabilisedData.csv", function(error, data) {
-    if (error) throw error;
-    console.log(data);
-    data.forEach(function(d) {
-        d.Year = d.Year;
-        d.Annomaly = +d.Annomaly;
-    });
-    stabilisedData = data;
-});
-
-
-/***** set axis for graph of historic + hot house data ****/
 var margin = {
         top: 60,
         right: 20,
@@ -39,24 +9,18 @@ var margin = {
     width = 400 - margin.left - margin.right,
     height = 365 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
-    .range([0, width]);
+var x = d3.scaleLinear().range([0, width]);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+var y = d3.scaleLinear().range([height, 0]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
+var xAxis = d3.axisBottom(x)
     .tickValues([1880, 2018, 2100])
-    .tickFormat(d3.format("d"))
-    .orient("bottom");
+    .tickFormat(d3.format("d"));
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .tickValues([0, 1, 3, 5])
-    .orient("left");
+var yAxis = d3.axisLeft(y)
+    .tickValues([0, 1, 3, 5]);
 
-var line = d3.svg.line()
+var line = d3.line()
     .x(function(d) {
         return x(d.Year);
     })
@@ -64,36 +28,62 @@ var line = d3.svg.line()
         return y(d.Annomaly);
     });
 
-// creates fixed values on the axis
-x.domain([1880, 2150]).nice();
-y.domain([0, 9]).nice();
-
-
-/****** sets graph of historic + hot house data *****/
-var graph = d3.select("#graph").append("svg")
+var svg = d3.select("#graph").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-graph.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+var allData;
 
-graph.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("temperature");
+d3.csv('data/temperatureData.csv')
+    .then(function(data) {
+        console.log(data);
+
+        data.forEach(function(d) {
+            d.Year = d.Year;
+            d.Annomaly = +d.Annomaly;
+        });
+
+        allData = data;
+
+        // creates fixed values on the axis
+        x.domain([1880, 2150]).nice();
+        y.domain([0, 9]).nice();
+
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("temperature");
+
+        // svg.append("path")
+        //     .data([data])
+        //     .attr("class", "line")
+        //     .attr("d", line);
+        // .call(transition);
+    })
+    .catch(function(error) {
+        throw error;
+    })
+
+var t = d3.transition()
+    .duration(750)
+    .ease(d3.easeLinear);
 
 function drawSection(data, line, id) {
-    graph.append("path")
-        .datum(data)
+    svg.append("path")
+        .data([data])
         .attr("class", "line")
         .attr("d", line)
         .attr("id", id)
@@ -106,14 +96,23 @@ function transition(path) {
     path.attr("stroke-dasharray", totalLength + " " + totalLength)
         .attr("stroke-dashoffset", totalLength)
         .transition()
-        .duration(3000)
-        .ease("linear")
+        .duration(2000)
+        .ease(d3.easeLinear)
         .attr("stroke-dashoffset", 0)
-        .each("end", restartScroll);
+        .on("end", restartScroll);
+
+    // path.transition()
+    //     .duration(2000)
+    //     .attrTween("stroke-dasharray", tweenDash);
 }
 
-
-/********* sets graph for stabilised data + historic ***********/
+// function tweenDash() {
+//     var l = this.getTotalLength(),
+//         i = d3.interpolateString("0," + l, l + "," + l);
+//     return function(t) {
+//         return i(t);
+//     };
+// }
 
 
 /************* Graph animations functions **************/
@@ -121,7 +120,7 @@ function transition(path) {
 function animateGraphSection1() {
     var start_index = 0;
     var end_index = 145;
-    var section = allTempData.slice(start_index, end_index);
+    var section = allData.slice(start_index, end_index);
     d3.select("#section2").remove();
     d3.select("#section1").remove();
     drawSection(section, line, "section1");
@@ -130,7 +129,7 @@ function animateGraphSection1() {
 function animateGraphSection2() {
     var start_index = 144;
     var end_index = 200;
-    var section = allTempData.slice(start_index, end_index);
+    var section = allData.slice(start_index, end_index);
 
     d3.select("#section3").remove();
     d3.select("#section2").remove();
@@ -140,7 +139,7 @@ function animateGraphSection2() {
 function animateGraphSection3() {
     var start_index = 199;
     var end_index = 234;
-    var section = allTempData.slice(start_index, end_index);
+    var section = allData.slice(start_index, end_index);
 
     d3.select("#section4").remove();
     d3.select("#section3").remove();
@@ -150,16 +149,12 @@ function animateGraphSection3() {
 function animateGraphSection4() {
     var start_index = 233;
     var end_index = 272;
-    var section = allTempData.slice(start_index, end_index);
+    var section = allData.slice(start_index, end_index);
 
     d3.select("#section4").remove();
     drawSection(section, line, "section4");
 };
 
-function animateGraphConclusion() {
-
-    $("#graph").css('z-index', '999');
-}
 
 /****************** Text animations ******************** */
 
@@ -223,12 +218,12 @@ $.scrollify({
         $(".panel1").append(pagination);
 
         /*
-			Tip: The two click events below are the same:
-			$(".pagination a").on("click",function() {
-			  $.scrollify.move($(this).attr("href"));
-			});
-	  
-			*/
+        Tip: The two click events below are the same:
+        $(".pagination a").on("click",function() {
+          $.scrollify.move($(this).attr("href"));
+        });
+  
+        */
         $(".pagination a").on("click", $.scrollify.move);
     },
     after: function(i, panels) {
@@ -259,9 +254,6 @@ $.scrollify({
 
             animateGraphSection4();
 
-        } else if (i == 5) {
-
-            animateGraphConclusion();
         }
     },
 });
@@ -290,30 +282,30 @@ function stopScroll() {
 
 
 /*
- * jQuery Scrollify
- * Version 1.0.19
- *
- * Requires:
- * - jQuery 1.7 or higher
- *
- * https://github.com/lukehaas/Scrollify
- *
- * Copyright 2016, Luke Haas
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- 
- */
+* jQuery Scrollify
+* Version 1.0.19
+*
+* Requires:
+* - jQuery 1.7 or higher
+*
+* https://github.com/lukehaas/Scrollify
+*
+* Copyright 2016, Luke Haas
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+* the Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
